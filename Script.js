eb3a0229bb6e4f1a02d8ce026b9fedb8a882f820
vicpezzjr <vhpezz@outlook.com>
2024-10-13 12:28:36 -0600

    Add files via upload

---
+++ Script.js
@@ -0,0 +1,11 @@
+// This script ensures smooth scrolling when clicking the arrow
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+      e.preventDefault();
+  
+      document.querySelector(this.getAttribute('href')).scrollIntoView({
+        behavior: 'smooth'
+      });
+    });
+  });
+  
